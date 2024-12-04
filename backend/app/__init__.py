7f01ec1d1a90aca02dc08eb8e2847518810f19c0
TheIceNarc <mattreiley12@gmail.com>
2024-12-04 13:57:41 -0800

    railway deployment features

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask_caching import Cache
+from flask_cors import CORS
 
 # Initialize the Flask app and Cache
 app = Flask(__name__)
+
+CORS(app, origins=["https://news-reader-now.vercel.app"])
+
+
 app.config['CACHE_TYPE'] = 'simple'  # You can choose 'simple', 'redis', 'filesystem', etc.
 app.config['CACHE_DEFAULT_TIMEOUT'] = 300  # Set your cache timeout, in seconds
 
