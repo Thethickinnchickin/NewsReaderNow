5817d1aba23d49f77caebe92a2609adc13b0de61
Matthew Reiley <mattreileydeveloper@gmail.com>
2025-06-27 14:30:41 -0700

    cors updated

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -8,8 +8,12 @@ nltk.download('punkt_tab')
 # Initialize the Flask app and Cache
 app = Flask(__name__)
 
-CORS(app, origins=["https://news-reader-now.vercel.app", "http://localhost:3000"], methods=["GET", "HEAD"])
-
+CORS(app, supports_credentials=True, resources={r"/api/*": {
+    "origins": [
+        "https://news-reader-now.vercel.app",
+        "http://localhost:3000"
+    ]
+}})
 
 # app.config['CACHE_TYPE'] = 'null'  # You can choose 'simple', 'redis', 'filesystem', etc.
 # app.config['CACHE_DEFAULT_TIMEOUT'] = 300  # Set your cache timeout, in seconds
